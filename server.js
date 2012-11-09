ac6082ec6c7835542e757b9099ed8a0871d81f92
Søren Kyndi-Wiuff <soren@sxl.dk>
2012-11-09 18:29:38 +0100

    Simplificeret server delen så den er specifikt rettet mod udviklingen. Produktion senere

--- server.js
+++ server.js
@@ -1,63 +1,104 @@
-(function(scope) {
-    scope.LOG_FORMAT = "{{timestamp}} - {{method}} {{status}} {{&url}} [{{&patterns}}]";
-    var fs = require("fs");
-    var http = require('http');
-    var util = require('util');
-    var path = require('path');
-    var Handler = require('./lib/handler.js').Handler;
-    var mustache = require('mustache');
-    var handlers = [];
+(function (scope) {
+    scope.LOG_FORMAT = "{{timestamp}} - {{method}} {{status}} {{&url}}";
+    var fs = require("fs"),
+        http = require('http'),
+        util = require('util'),
+        path = require('path'),
+        mustache = require('mustache'),
+        sass = require('./lib/sass'),
+        mime = require('mime'),
+        indexer = require('./lib/indexer');
 
-    function serve404(req, res) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        var msg404 = ""+req.url + " findes ikke";
-        res.end(msg404);
-    }
-    function handleRequest(req, res) {
-
+    function sendResponse(res, status, mimeType, msg) {
+        res.writeHead(status, {'Content-Type':mimeType});
+        res.end("" + msg);
     }
 
-    scope.serve = function (port) {
-        scope.server = http.createServer( function(req, res) {
-            var chosen = null;
-            req.on('end', function() {
-                console.log(mustache.to_html(scope.LOG_FORMAT, {
-                    method : req.method,
-                    url : req.url,
-                    status : res.statusCode,
-                    timestamp : new Date().toISOString(),
-                    patterns : chosen ? chosen.patterns() : '404'
-                }));
-                return true;
-            });
+    function registerLogging(req, res) {
+        req.on('end', function () {
+            console.log(mustache.to_html(scope.LOG_FORMAT, {
+                method:req.method,
+                url:req.url,
+                status:res.statusCode,
+                timestamp:new Date().toISOString()
+            }));
+            return true;
+        });
+    }
 
-            var matched = false;
-            handlers.forEach(function(handler) {
-                if( !matched ) {
-                    if( handler.match(req.url ) ) {
-                        matched = true;
-                        chosen = handler;
-                        handler.run(req,res);
-                    }
-                }
-            });
+    function staticFiles(req, res) {
+        var file = this.basePath + req.url;
+        fs.readFile(file, 'UTF-8', function (err, data) {
+            if (err) {
+                sendResponse(res, 404, 'text/plain', "Couldn't find " + req.url + "\nError: " + err);
+            } else {
+                sendResponse(res, 200, mime.lookup(file), data);
+            }
+        })
+    }
 
-            if( !matched ) {
-                serve404(req,res);
+    function scssTemplate(req, res) {
+        var files = indexer.indexFiles(this.basePath);
+        var libs = [];
+        for (var i = 0; i < files['scss'].length; i++) {
+            var file = files['scss'][i];
+            libs.push(path.dirname(file));
+        }
+        sass.render(this.basePath + req.url, libs, function (err, css) {
+            if (err) {
+                sendResponse(res, 500, 'text/plain', err);
+            } else {
+                sendResponse(res, 200, 'text/css', css);
             }
         });
-        scope.server.listen(port);
-
-    };
-    scope.registerHandler = function(pattern, handler) {
-        scope.addHandler(new Handler(pattern, handler));
-    };
-    scope.addHandler = function (handler) {
-        handlers.push(handler);
-    };
-    scope.registerDev = function(basePath) {
-        var dev = require('./lib/dev');
-        dev.setBasePath(basePath);
-        dev.register(scope);
     }
+
+    function sendIndexFile(req, res) {
+        var files = indexer.indexFiles(this.basePath);
+        var model = {
+            js:indexer.replaceList(this.basePath, '', files['js']),
+            css:indexer.replaceList(this.basePath, '', files['scss'])
+        };
+
+        fs.readFile(this.basePath + '/index.html', 'UTF-8', function (err, data) {
+            if (err) {
+                sendResponse(res, 500, 'text/plain', err);
+            } else {
+                var html = mustache.to_html(data, model);
+                sendResponse(res, 200, 'text/html; charset=UTF-8', html);
+            }
+        });
+    }
+
+    function isStaticFile(url) {
+        var matched = false;
+        ['/libs', '/js', '/app', '/img'].forEach(function (item) {
+            matched = url.indexOf(item) == 0 ? true : matched;
+        });
+        if (matched) {
+            matched =  !isScssFile(url);
+        }
+        return matched;
+    }
+
+    function isScssFile(url) {
+        return url.match(".*\.scss$");
+    }
+
+    scope.DevServer = function (port, basePath) {
+        this.basePath = path.resolve(basePath);
+        var dev = this;
+        this.server = http.createServer(function (req, res) {
+            registerLogging(req, res);
+            if (isStaticFile(req.url)) {
+                staticFiles.call(dev,req, res);
+            } else if (isScssFile(req.url)) {
+                scssTemplate.call(dev,req, res);
+            } else {
+                sendIndexFile.call(dev, req, res);
+            }
+        });
+        this.server.listen(port);
+    };
+
 })(exports)
